04b09864910a5ac5c6a237fa9269ecfbb382db37
Kerry W. Lothrop <kerry@lothrop.de>
2014-12-16 21:58:30 +0100

    Added application lifetime objects.

--- NuSpec/MvvmCross/WindowsPhoneContent/App.xaml.cs.pp
+++ NuSpec/MvvmCross/WindowsPhoneContent/App.xaml.cs.pp
@@ -88,6 +88,24 @@ namespace $rootnamespace$
             }
         }
 
+        // Code to execute when the application is activated (brought to foreground)
+        // This code will not execute when the application is first launched
+        private void Application_Activated(object sender, ActivatedEventArgs e)
+        {
+        }
+
+        // Code to execute when the application is deactivated (sent to background)
+        // This code will not execute when the application is closing
+        private void Application_Deactivated(object sender, DeactivatedEventArgs e)
+        {
+        }
+
+        // Code to execute when the application is closing (eg, user hit Back)
+        // This code will not execute when the application is deactivated
+        private void Application_Closing(object sender, ClosingEventArgs e)
+        {
+        }
+
         // Code to execute on Unhandled Exceptions
         private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
         {
